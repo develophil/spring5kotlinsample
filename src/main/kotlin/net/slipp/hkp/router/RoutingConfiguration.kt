f808b8217b0bb9edcd795ae2892bd9676f410db1
Unknown <hkpking@tmon.co.kr>
2018-02-11 20:15:08 +0900

    racing router 추가
    index, game, result 템플릿 연결

--- src/main/kotlin/net/slipp/hkp/router/RoutingConfiguration.kt
+++ src/main/kotlin/net/slipp/hkp/router/RoutingConfiguration.kt
@@ -3,6 +3,7 @@ package net.slipp.hkp.router
 import net.slipp.hkp.handler.HelloWorldHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.body
@@ -13,11 +14,20 @@ class RoutingConfiguration {
     @Bean
     fun routeFunction(handler: HelloWorldHandler) : RouterFunction<ServerResponse> = router {
         ("/").nest {
-            GET("/route") { req ->
+            GET("/helloworld") { req ->
                 ServerResponse.ok().body(
                         handler.helloworld()
                 )
             }
+        }
+    }
+
+    @Bean
+    fun racingRouter() : RouterFunction<ServerResponse> = router {
+        accept(MediaType.TEXT_HTML).nest {
+            GET("/racing") { ServerResponse.ok().render("index") }
+            GET("/racing/game") { ServerResponse.ok().render("game") }
+            GET("/racing/result") { ServerResponse.ok().render("result") }
         }
     }
 }
