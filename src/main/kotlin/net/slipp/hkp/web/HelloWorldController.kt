e33138c899a25620bfae9c5c653ddf5d056ec072
Unknown <hkpking@tmon.co.kr>
2018-02-11 19:01:23 +0900

    sample controller 추가 - Mono<String>

--- src/main/kotlin/net/slipp/hkp/web/HelloWorldController.kt
+++ src/main/kotlin/net/slipp/hkp/web/HelloWorldController.kt
@@ -2,11 +2,18 @@ package net.slipp.hkp.web
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RestController
 class HelloWorldController {
-    @GetMapping("/helloworld")
+    @GetMapping("/default")
     fun hello(): String {
         return "Hello World!"
+    }
+
+
+    @GetMapping("/mono")
+    fun helloMono(): Mono<String> {
+        return Mono.just("Hello world!!!!")
     }
 }
