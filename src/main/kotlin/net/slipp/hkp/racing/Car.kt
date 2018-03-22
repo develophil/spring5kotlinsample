0e697f979fca37f663be1853e9db6d648ea4097b
Unknown <hkpking@tmon.co.kr>
2018-03-22 23:00:19 +0900

    override car's equals and hashcode

--- src/main/kotlin/net/slipp/hkp/racing/Car.kt
+++ src/main/kotlin/net/slipp/hkp/racing/Car.kt
@@ -1,3 +1,12 @@
 package net.slipp.hkp.racing
 
-data class Car(var name: String)
+data class Car(var name: String) {
+
+    override fun equals(other: Any?): Boolean {
+        return name.equals((other as Car).name)
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+}
