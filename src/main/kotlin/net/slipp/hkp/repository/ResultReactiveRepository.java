b307e80fe008538982164422056bb313787dcf6e
Unknown <hkpking@tmon.co.kr>
2018-04-02 21:40:07 +0900

    mongoDB sample 추가

---
+++ src/main/kotlin/net/slipp/hkp/repository/ResultReactiveRepository.java
@@ -0,0 +1,13 @@
+package net.slipp.hkp.repository;
+
+import net.slipp.hkp.racing.Car;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+
+public interface ResultReactiveRepository extends ReactiveCrudRepository<Car, String> {
+
+	@Query("{ id: { $exists: true }}")
+	Flux<Car> retrieveAllCarPaged(final Pageable page);
+}
--- src/main/kotlin/net/slipp/hkp/web/HelloWorldController.kt
+++ src/main/kotlin/net/slipp/hkp/web/HelloWorldController.kt
@@ -1,5 +1,8 @@
 package net.slipp.hkp.web
 
+import net.slipp.hkp.racing.Car
+import net.slipp.hkp.repository.ResultReactiveRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,9 +12,16 @@ import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.Duration
+import org.springframework.data.domain.PageRequest
+import org.springframework.web.bind.annotation.RequestParam
+
+
 
 @RestController
-class HelloWorldController() {
+class HelloWorldController(private val resultReactiveRepository: ResultReactiveRepository) {
+
+    private val DELAY_PER_ITEM_MS = 100
+
     @GetMapping("/default")
     fun hello(): String {
         return "Hello World!"
@@ -44,6 +54,22 @@ class HelloWorldController() {
         return Flux.zip( interval, a ).map{it.t2}
     }
 
+    @PostMapping("/car-reactive")
+    fun saveCarFlux(): Mono<Car> {
+        return resultReactiveRepository.save(Car("test!"))
+    }
+
+    @GetMapping("/car-reactive")
+    fun getCarFlux(): Flux<Car> {
+        return resultReactiveRepository.findAll().delayElements(Duration.ofMillis(DELAY_PER_ITEM_MS.toLong()))
+    }
+
+    @GetMapping("/car-reactive-paged")
+    fun getCarFlux(@RequestParam(name = "page") page: Int,
+                     @RequestParam(name = "size") size: Int): Flux<Car> {
+        return resultReactiveRepository.retrieveAllCarPaged(PageRequest.of(page, size))
+                .delayElements(Duration.ofMillis(DELAY_PER_ITEM_MS.toLong()))
+    }
 }
 
 
